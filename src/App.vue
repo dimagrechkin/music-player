00b462316330afb5ba99d4621bd7a02f37dcf01c
Dima <hrechkin.dmytro@nixsolutions.com>
2023-01-10 18:24:43 +0100

    setup: add basic setup, add structure, alias, prettier, eslint

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <app-layout>
+    <template #header> header </template>
+    <template #sidebar> sidebar </template>
+    <template #main>
+      <router-view></router-view>
+    </template>
+  </app-layout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from '@/components/AppLayout.vue';
+</script>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-content">
+    <slot name="sidebar"></slot>
+
+    <slot name="header"></slot>
+    <div class="main-scroll">
+      <slot name="main"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
