a57906e3c1c8cc4791f2476c788282d34a35d813
Dima <hrechkin.dmytro@nixsolutions.com>
2023-03-03 15:35:38 +0100

    add blockchain development enviroment Lens Protocol to project, split the  project into frontend and blcokhain

--- frontend/src/utils/lens-hub.ts
+++ frontend/src/utils/lens-hub.ts
@@ -5,6 +5,3 @@ import { getSigner } from './helpers';
 // lens contract info can all be found on the deployed
 // contract address on polygon.
 export const lensHub = new ethers.Contract(LENS_CONTRACT_ADDRESS, LENS_CONTRACT_ABI, getSigner());
-console.log(lensHub, 'lensHub');
-
-// export const lensPeriphery = new ethers.Contract(LENS_PERIPHERY_CONTRACT, LENS_PERIPHERY_ABI, getSigner());
